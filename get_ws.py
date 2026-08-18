95cc7155fc5732dd7d164ce5d2f31b97443e328e
Poppop85 <kevincasp@hotmail.com>
2026-08-19 07:55:20 +0900

    Clean up test scripts

--- get_ws.py
+++
@@ -1,9 +0,0 @@
-import json, urllib.request
-
-resp = urllib.request.urlopen('http://127.0.0.1:9222/json')
-data = json.loads(resp.read())
-for tab in data:
-    if tab.get('type') == 'page' and 'poppop85' in tab.get('url', ''):
-        print(f"WS: {tab.get('webSocketDebuggerUrl')}")
-        print(f"URL: {tab['url']}")
-        print(f"ID: {tab['id']}")
